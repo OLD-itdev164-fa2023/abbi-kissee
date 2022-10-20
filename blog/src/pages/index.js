b266af804befa665408100d6c9ae756b00251523
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-10-20 16:28:12 -0500

    Completed Activity 6

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,53 +4,60 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from '../components/List'
 import * as styles from "../components/index.module.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <ul className={styles.list}>
+    <List width={[1, 2/3, 7/8]} p={2}>
       {
         data.allContentfulBlogPost.edges.map(edge => (
-          <li key={edge.node.id}>
+          <ListItem p={3} key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
             <div>
               <GatsbyImage
-                image={edge.node.heroImagegatsbyImageData}
+                image={edge.node.heroImage.gatsbyImageData}
               />
             </div>
             <div>
               {edge.node.body.childMarkdownRemark.excerpt}
             </div>
-          </li>
+          </ListItem>
         ))
 
       }
-    </ul>
+    </List>
   </Layout>
 )
 
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
 
 export const query = graphql`
-  { 
+  {
     allContentfulBlogPost {
       edges {
         node {
-          id
-          slug
+          id 
+          title
+          slug 
           body {
-            childMarkdownRemark {
-              excerpt
+						childMarkdownRemark {
+							excerpt
             }
           }
           heroImage {
-            gatsbyImageData(
-                layout: CONSTRAINED
-                placeholder: BLURRED
-                width: 300
+						gatsbyImageData(
+							layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 600
             )
           }
         }
